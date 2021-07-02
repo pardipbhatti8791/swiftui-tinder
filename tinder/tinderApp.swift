47fa02d08c169449f4de1285c63b481177998f06
Pardip Bhatti <pardipbhatti8791@gmail.com>
2021-07-02 13:17:01 +0530

    Initial Commit

---
+++ tinder/tinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinderApp.swift
+//  tinder
+//
+//  Created by  GPCODERS on 02/07/21.
+//
+
+import SwiftUI
+
+@main
+struct tinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
